23f64ab5512e80b6769a435d8226a5772bb63106
Kiarash Korki <kiarash96@users.sf.net>
2015-05-23 15:36:19 +0430

    Implemented double buffering and fixed fps

--- src/bobby/SceneManager.java
+++ src/bobby/SceneManager.java
@@ -2,7 +2,10 @@ package bobby;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JApplet;
 
 /**
@@ -11,25 +14,37 @@ import javax.swing.JApplet;
  */
 public class SceneManager extends Thread {
 	
+	Image buffer; // second buffer
+	
 	private final JApplet parent;
 	private ArrayList<SceneObject> list;
 	
 	public SceneManager(JApplet parent) {
 		list = new ArrayList<>();
 		this.parent = parent;
+		
+		buffer = parent.createImage(parent.getWidth(), parent.getHeight());
 	}
 	
 	public void draw(Graphics g) {
-		// TODO: implement double buffering
+		Graphics offG = buffer.getGraphics();
 		
 		// clear the screen
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, parent.getWidth(), parent.getHeight());
+		offG.setColor(Color.WHITE);
+		offG.fillRect(0, 0, parent.getWidth(), parent.getHeight());
 		
 		for (SceneObject sobject : list)
-			sobject.draw(g);
+			sobject.draw(offG);
 		
-		// TODO: sleep
+		// draw second buffer on the applet
+		g.drawImage(buffer, 0, 0, parent);
+		
+		try {
+			Thread.sleep(1000/60); // 60 fps
+		}
+		catch (InterruptedException ex) {
+			Logger.getLogger(SceneManager.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 	
 	@Override
