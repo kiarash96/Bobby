7be2cad0c3747b736445f82a9d5c3d21b1d86454
Kiarash Korki <kiarash96@users.sf.net>
2015-06-05 20:42:22 +0430

    removed unused imports

--- src/bobby/scene/Player.java
+++ src/bobby/scene/Player.java
@@ -28,11 +28,6 @@ import bobby.main.KeyHandler;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 
 /**
  *
